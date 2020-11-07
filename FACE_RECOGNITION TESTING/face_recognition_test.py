562e9974863ebfb7f1c9624dae18cb7b23ba8ea1
Sean Ashley <sean.d.ashley@gmail.com>
2020-11-07 02:25:22 -0500

    FEAT : Added proof of concept of live stream facial detection     Got facial detection running properly, still need to figure out     how we can have an output saying if the face is there. 	new file:   FACE_RECOGNITION TESTING/face_recognition_test.py

---
+++ FACE_RECOGNITION TESTING/face_recognition_test.py
@@ -0,0 +1,49 @@
+#proof of concept code , adapted from https://github.com/ageitgey/face_recognition/blob/master/examples/facerec_from_webcam_faster.py#L60
+
+import face_recognition
+import cv2
+import numpy as np
+
+video_capture = cv2.VideoCapture(0)
+
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+process_this_frame = True
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = frame[:, :, ::-1]
+
+    # Only process every other frame of video to save time
+
+
+    face_locations = face_recognition.face_locations(rgb_small_frame,model="cnn")
+
+    # Display the results
+    for (top, right, bottom, left) in face_locations:
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, "Sean" ,  (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
